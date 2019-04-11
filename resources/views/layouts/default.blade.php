7a09455a14ffde9ed3948cee4d222b439df75ff0
Nguyen Thanh <nguyen.thanh@alliedtechcamp.com>
2019-04-11 15:20:43 +0700

    update source

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,6 +9,7 @@
     <meta property="og:description" content="@yield('description')">
     <meta property="og:image" content="@yield('image')" />
     <link rel="icon" href="{{ asset('favicon.png') }}" />
+    <meta name="google-site-verification" content="xVR-3NTFIzHZbeNT_0VJc2a9t3W5dIwrszUBbruFnx8" />
     @section('header_css')
     @show
 </head>
