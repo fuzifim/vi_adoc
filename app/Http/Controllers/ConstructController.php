0f1965e75a2e21c6d4dd1780bc3b8f57f43d4231
Nguyen Thanh <nguyen.thanh@alliedtechcamp.com>
2019-04-11 09:18:34 +0700

    update source

--- app/Http/Controllers/ConstructController.php
+++ app/Http/Controllers/ConstructController.php
@@ -22,7 +22,7 @@ class ConstructController extends Controller
         $this->_keywordPrimary = Cache::store('memcached')->remember('keyword_primary', 1, function()
         {
             return DB::connection('mongodb')->collection('mongo_keyword')
-                ->where('lang',env('LANG'))
+                ->where('lang',config('app.locale'))
                 ->where('parent',null)
                 ->simplePaginate(10);
         });
