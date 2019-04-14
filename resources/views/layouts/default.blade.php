b4c745f0a24b2eb574b831688abda45c67ef09f0
MacOS13 <macos13@MacBook-Pro-cua-MacOS13.local>
2019-04-14 15:56:54 +0700

    update source

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>@yield('title')</title>
     @if (View::hasSection('setCanonical'))
     <link rel="canonical" href="@yield('setCanonical')" />
